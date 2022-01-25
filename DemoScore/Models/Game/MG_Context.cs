3cf50f054a686f482b66baf7aacf21e1e18675b9
deyvidmartinez <deyvid.martinez.u@gmail.com>
2022-01-25 16:21:42 -0500

    Subida del proyecto al repositorio de GIT

---
+++ DemoScore/Models/Game/MG_Context.cs
@@ -0,0 +1,33 @@
+﻿using SCORM1.Models.MainGame;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace DemoScore.Models.Game
+{
+    public class MG_Context
+    {
+        [Key]
+        public int Contex_Id { get; set; }
+
+        [Display(Name = "Descripción Contexto")]
+        public string Contex_Desc { get; set; }
+
+        [Display(Name = "Nombre Competencia")]
+        public string Contex_Compet { get; set; }
+
+        [Display(Name = "Fuente o Autor")]
+        public string Contex_Autor { get; set; }
+
+        [ForeignKey("Company")]
+        [Display(Name = "Compañia")]
+        public int Company_Id { get; set; }
+
+        public Company Company { get; set; }
+
+        public virtual ICollection<MG_MultipleChoice> MG_MultipleChoice { get; set; }
+    }
+}
